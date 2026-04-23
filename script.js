c03e340bb6e96d8874b26666d7dba69d948cb82f
Darren Corbillon <markdarrencorbillon@gmail.com>
2026-04-23 17:56:54 +0800

    WEB ver2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+// Keep your existing scroll code
 window.addEventListener('scroll', function() {
-        const navbar = document.querySelector('.nav');
-        // Kapag nag-scroll ka ng higit sa 50 pixels pababa...
-        if (window.scrollY > 50) {
-            navbar.classList.add('scrolled');
-        } else {
-            // Kapag bumalik ka sa pinakataas...
-            navbar.classList.remove('scrolled');
-        }
-    });
+    const navbar = document.querySelector('.nav');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
